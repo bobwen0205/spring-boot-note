a485b8670829a34f89594a676edead37af0c2639
bobwen0205 <wenmingzhi10@gmail.com>
2025-10-29 20:28:26 -0400

    third commit

---
+++ 10-spring-boot-aop/spring-boot-mvc-crud-demo-employees/src/main/java/com/bobwen/springboot/thymeleafdemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.bobwen.springboot.thymeleafdemo.service;
+
+import com.bobwen.springboot.thymeleafdemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    void save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
